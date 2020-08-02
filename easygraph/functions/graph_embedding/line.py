ad369ccd323731db862ed29b02b9d078a2701166
ICYPOLE <39186199+ICYPOLE@users.noreply.github.com>
2020-08-02 20:41:24 +0800

    Fixed a bug

--- easygraph/functions/graph_embedding/line.py
+++ easygraph/functions/graph_embedding/line.py
@@ -197,7 +197,7 @@ class LINE:
 
     def train(self, batch_size=1024, epochs=1, initial_epoch=0, verbose=1, times=1):
         self.reset_training_config(batch_size, times)
-        hist = self.model.fit_generator(self.batch_it, epochs=epochs, initial_epoch=initial_epoch, steps_per_epoch=self.steps_per_epoch,
+        hist = self.model.fit(self.batch_it, epochs=epochs, initial_epoch=initial_epoch, steps_per_epoch=self.steps_per_epoch,
                                         verbose=verbose)
         return hist
         
