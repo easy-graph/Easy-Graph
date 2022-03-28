5cc44b79e33f86c022504d43ee9ccf33e5d36181
Teddy / Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-03-28 22:32:18 +0800

    Add check_installation.py

---
+++ scripts/check_installation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import easygraph as eg
+
+G = eg.Graph()
+
+G.add_edge(1, 2)
+assert G.edges == [(1, 2, {})]
+
+G.add_node('hello world')
+G.add_node('Jack', node_attr={'age': 10, 'gender': 'M'})
+assert G.nodes == {
+    1: {},
+    2: {},
+    'hello world': {},
+    'Jack': {
+        'node_attr': {
+            'age': 10,
+            'gender': 'M'
+        }
+    }
+}
