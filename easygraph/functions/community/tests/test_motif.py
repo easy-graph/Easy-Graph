cecb289189c0ce5163298af7ef595f614ee9564c
coreturn <863813609@qq.com>
2022-03-30 19:24:50 +0800

    fix: constraint install_requires

--- easygraph/functions/community/tests/test_motif.py
+++ easygraph/functions/community/tests/test_motif.py
@@ -10,6 +10,7 @@ class TestMotif(object):
 
     def test_esu(self):
         res = eg.enumerate_subgraph(self.G, 3)
+        res = [list(x) for x in res]
         exp_res = [{1, 3, 4}, {1, 2, 3}, {1, 3, 5}, {2, 3, 5}, {2, 3, 4}, {3, 4, 5}]
-        assert res == exp_res
-        print(res)
+        exp_res = [list(x) for x in exp_res]
+        assert sorted(res) == sorted(exp_res)
