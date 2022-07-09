ad1c742d92725221b414eee23a0c9c2d56d0e20c
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-07-10 00:37:58 +0800

    Bump version to 0.2a39

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if uname[0] == "Darwin" or uname[0] == "Linux":
 CYTHON_STR = 'Cython'
 
 setuptools.setup(name="Python-EasyGraph",
-                 version="0.2a38",
+                 version="0.2a39",
                  author="Fudan MSN Group",
                  author_email="easygraph@163.com",
                  description="Easy Graph",
