b0b836b634506d5ba20be1b9332841c0d3d65648
yizhihenpidehou <806237230@qq.com>
2022-09-07 11:39:38 +0800

    add cpp_prim_mst_edges

--- cpp_easygraph/functions/not_sorted/mst.cpp
+++ cpp_easygraph/functions/not_sorted/mst.cpp
@@ -1,11 +1,47 @@
 #include "mst.h"
 
-<<<<<<< HEAD
 #include <pybind11/stl.h>
 
 #include <cmath>
 
 #include "../../classes/graph.h"
+UnionFind::UnionFind(std::vector<node_t> elements) {
+    for (node_t x : elements) {
+        parents[x] = x;
+        weights[x] = 1;
+    }
+}
+
+node_t UnionFind::operator[](node_t object) {
+    if (!parents.count(object)) {
+        parents[object] = object;
+        weights[object] = 1;
+        return object;
+    }
+
+    std::vector<node_t> path;
+    path.push_back(object);
+    node_t root = parents[object];
+    while (root != path.back()) {
+        path.push_back(root);
+        root = parents[root];
+    }
+    for (node_t ancestor : path) {
+        parents[ancestor] = root;
+    }
+    return root;
+}
+
+void UnionFind::_union(node_t object1, node_t object2) {
+    node_t root, r;
+    if (weights[object1] < weights[object2]) {
+        root = object1, r = object2;
+    } else {
+        root = object2, r = object1;
+    }
+    weights[root] += weights[r];
+    parents[r] = root;
+}
 struct mst_Edge {
     double wt;
     node_t start_node, end_node;
@@ -27,60 +63,64 @@ py::object cpp_prim_mst_edges(py::object G, py::object minimum, py::object weigh
     py::list res = py::list();
     node_dict_factory nodes_list = G_.node;
     std::unordered_set<node_t> nodes;
-
     for (node_dict_factory::iterator iter = nodes_list.begin(); iter != nodes_list.end(); iter++) {
         node_t node_id = iter->first;
         nodes.emplace(node_id);
     }
     int sign = 1;
-    if (minimum) {
-        std::cout << "minimum:" << minimum << std::endl;
+    if (!minimum.cast<bool>()) {
         sign = -1;
     }
     while (!nodes.empty()) {
-        // 随机从nodes中pop出一个元素
-        node_t u = *nodes.begin();
+        const node_t u = *(nodes.begin());
         nodes.erase(nodes.begin());
         std::priority_queue<mst_Edge, std::vector<mst_Edge>, cmp> frontier;
         std::unordered_map<node_t, bool> visited;
-        visited.emplace(std::make_pair(u, true));
+        node_t u_ = u;
+        visited.emplace(u_, true);
         adj_attr_dict_factory u_neighbors = G_.adj[u];
         for (adj_attr_dict_factory::iterator i = u_neighbors.begin(); i != u_neighbors.end(); i++) {
             node_t v = i->first;
             edge_attr_dict_factory d = i->second;
             double wt = sign;
-            std::cout << "weight:" << py::cast<std::string>(weight) << std::endl;
             if (d.find(py::cast<std::string>(weight)) != d.end()) {
                 wt = d[py::cast<std::string>(weight)] * sign;
             }
             if (isnan(wt)) {
-                //
-                if (ignore_nan) {
+                if (ignore_nan.cast<bool>()) {
                     continue;
                 }
-
-                PyErr_Format(PyExc_ValueError, "NaN found as an edge weight. Edge {(%R %R %R)}", (G_.id_to_node.attr("get")(u)).ptr(), G_.id_to_node.attr("get")(v).ptr(), &d);
+                py::dict temp_attr = py::dict();
+                for (edge_attr_dict_factory::iterator j = d.begin(); j != d.end(); j++) {
+                    temp_attr[py::cast(j->first)] = py::cast(j->second);
+                }
+                PyErr_Format(PyExc_ValueError, "NaN found as an edge weight. Edge {(%R %R %R)}", (G_.id_to_node.attr("get")(u)).ptr(), G_.id_to_node.attr("get")(v).ptr(), temp_attr.ptr());
+                return py::none();
             }
-            frontier.push(mst_Edge(wt, u, v, d));
+            frontier.push(mst_Edge(wt, u_, v, d));
         }
         while (!frontier.empty()) {
             mst_Edge node = frontier.top();
             frontier.pop();
             double W = node.wt;
-            node_t u = node.start_node;
-            node_t v = node.end_node;
+            node_t u_id = node.start_node;
+            node_t v_id = node.end_node;
             edge_attr_dict_factory d = node.edge_attr;
-            if (visited.find(v) != visited.end() || nodes.find(v) == nodes.end()) {
+            if (visited.find(v_id) != visited.end() || nodes.find(v_id) == nodes.end()) {
                 continue;
             }
-            if (data) {
-                res.append(py::make_tuple(G_.id_to_node.attr("get")(u), G_.id_to_node.attr("get")(v), d));
+            if (data.cast<bool>()) {
+                py::dict temp_attr = py::dict();
+                for (edge_attr_dict_factory::iterator j = d.begin(); j != d.end(); j++) {
+                    temp_attr[py::cast(j->first)] = py::cast(j->second);
+                }
+                res.append(py::make_tuple(G_.id_to_node.attr("get")(u_id), G_.id_to_node.attr("get")(v_id), temp_attr));
             } else {
-                res.append(py::make_tuple(G_.id_to_node.attr("get")(u), G_.id_to_node.attr("get")(v)));
+                res.append(py::make_tuple(G_.id_to_node.attr("get")(u_id), G_.id_to_node.attr("get")(v_id)));
             }
-            visited.emplace(v);
-            nodes.erase(v);
-            adj_attr_dict_factory v_neighbors = G_.adj[v];
+            visited.emplace(v_id, true);
+            nodes.erase(v_id);
+            adj_attr_dict_factory v_neighbors = G_.adj[v_id];
             for (adj_attr_dict_factory::iterator j = v_neighbors.begin(); j != v_neighbors.end(); j++) {
                 node_t w = j->first;
                 edge_attr_dict_factory d2 = j->second;
@@ -91,47 +131,9 @@ py::object cpp_prim_mst_edges(py::object G, py::object minimum, py::object weigh
                 if (d2.find(py::cast<std::string>(weight)) != d2.end()) {
                     new_weight = d2[py::cast<std::string>(weight)] * sign;
                 }
-                frontier.push(mst_Edge(new_weight,v,w,d2));
+                frontier.push(mst_Edge(new_weight, v_id, w, d2));
             }
         }
     }
-=======
-UnionFind::UnionFind(std::vector<node_t> elements = std::vector<node_t>()) {
-	for (node_t x : elements) {
-		parents[x] = x;
-		weights[x] = 1;
-	}
-}
-
-node_t UnionFind::operator[](node_t object) {
-	if (!parents.count(object)) {
-		parents[object] = object;
-		weights[object] = 1;
-		return object;
-	}
-
-	std::vector<node_t> path;
-	path.push_back(object);
-	node_t root = parents[object];
-	while (root != path.back()) {
-		path.push_back(root);
-		root = parents[root];
-	}
-	for (node_t ancestor : path) {
-		parents[ancestor] = root;
-	}
-	return root;
-}
-
-void UnionFind::_union(node_t object1, node_t object2) {
-	node_t root, r;
-	if (weights[object1] < weights[object2]) {
-		root = object1, r = object2;
-	}
-	else {
-		root = object2, r = object1;
-	}
-	weights[root] += weights[r];
-	parents[r] = root;
->>>>>>> 7943fe837953dcfff3701edddb55528f23574b45
+    return res;
 }
--- cpp_easygraph/functions/not_sorted/mst.h
+++ cpp_easygraph/functions/not_sorted/mst.h
@@ -3,17 +3,8 @@
 #include "../../common/common.h"
 
 class UnionFind {
-<<<<<<< HEAD
-   public:
-    UnionFind(py::object elements = py::none());
-    node_t operator[](node_t node);
-    void _union(node_t node1, node_t node2);
-};
-
-py::object cpp_prim_mst_edges(py::object G, py::object minimum, py::object weight, py::object data, py::object ignore_nan);
-=======
 public:
-	UnionFind(std::vector<node_t> elements);
+	UnionFind(std::vector<node_t> elements= std::vector<node_t>());
 	node_t operator[](node_t object);
 	void _union(node_t object1, node_t object2);
 
@@ -21,4 +12,5 @@ private:
 	std::unordered_map<node_t, node_t> parents;
 	std::unordered_map<node_t, unsigned int> weights;
 };
->>>>>>> 7943fe837953dcfff3701edddb55528f23574b45
+
+py::object cpp_prim_mst_edges(py::object G, py::object minimum, py::object weight, py::object data, py::object ignore_nan);
