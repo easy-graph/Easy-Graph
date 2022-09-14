0c1340c746fa3277020de801ad5f1840a9384ff5
yizhihenpidehou <806237230@qq.com>
2022-09-14 16:57:19 +0800

    fix:connected_component

--- cpp_easygraph/functions/components/connected.cpp
+++ cpp_easygraph/functions/components/connected.cpp
@@ -9,27 +9,29 @@ py::object plain_bfs(py::object G, py::object source) {
     Graph& G_ = G.cast<Graph&>();
     node_t source_id = G_.node_to_id.attr("get")(source).cast<node_t>();
     adj_dict_factory& G_adj = G_.adj;
-    std::set<node_t> seen;
-    std::set<node_t> nextlevel;
+    std::unordered_set<node_t> seen;
+    std::unordered_set<node_t> nextlevel;
     nextlevel.emplace(source_id);
     py::list res = py::list();
     while (nextlevel.size()) {
-        std::set<node_t> thislevel = nextlevel;
-        nextlevel = std::set<node_t>();
-        for (std::set<node_t>::iterator i = thislevel.begin(); i != thislevel.end(); i++) {
+        std::unordered_set<node_t> thislevel = nextlevel;
+        nextlevel = std::unordered_set<node_t>();
+        for (std::unordered_set<node_t>::iterator i = thislevel.begin(); i != thislevel.end(); i++) {
             node_t v_id = *i;
             if (seen.find(v_id) == seen.end()) {
-                res.append(G_.id_to_node.attr("get")(v_id));
                 seen.emplace(v_id);
                 adj_attr_dict_factory v_adj = G_adj[v_id];
                 for (adj_attr_dict_factory::iterator j = v_adj.begin(); j != v_adj.end(); j++) {
                     node_t neighbor_id = j->first;
-                    if (seen.find(neighbor_id) == seen.end()) {
-                        nextlevel.emplace(neighbor_id);
-                    }
+                    nextlevel.emplace(neighbor_id);
                 }
             }
         }
+    }
+
+    for (std::unordered_set<node_t>::iterator i = seen.begin(); i != seen.end(); i++) {
+        node_t res_id = *(i);
+        res.append(G_.id_to_node.attr("get")(res_id));
     }
     return res;
 }
