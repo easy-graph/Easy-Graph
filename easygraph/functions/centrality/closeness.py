d53044bc69877af0ed0d17fd2fb4ff4f28f0b81d
yizhihenpidehou <806237230@qq.com>
2022-10-27 17:18:23 +0800

    fixed:import error in closeness.py

--- easygraph/functions/centrality/closeness.py
+++ easygraph/functions/centrality/closeness.py
@@ -1,6 +1,5 @@
-from easygraph import single_source_bfs
-from easygraph import single_source_dijkstra
-from easygraph.functions.basic import *
+from easygraph.functions.path import single_source_bfs
+from easygraph.functions.path import single_source_dijkstra
 from easygraph.utils import *
 
 
