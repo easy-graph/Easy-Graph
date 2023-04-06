9438557907e6635c8267983e696acc624b61f035
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-04-06 12:27:48 +0800

    2023-04-06T12-27-48Z

--- setuppy-install.sh
+++ setuppy-install.sh
@@ -4,3 +4,5 @@ alias 'gcc'='/usr/local/Cellar/gcc/12.2.0/bin/gcc-12'
 alias 'g++'='/usr/local/Cellar/gcc/12.2.0/bin/g++-12'
 
 CC='/usr/local/Cellar/gcc/12.2.0/bin/gcc-12' CXX='/usr/local/Cellar/gcc/12.2.0/bin/g++-12' python3 setup.py build_ext
+python3 setup.py install --skip-build
+
