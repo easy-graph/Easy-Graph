6fca6119288c363ee19262e7e6e40a68a81d176d
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-04-14 21:29:26 +0800

    add k_core python implementation and test

--- easygraph/functions/core/k_core.py
+++ easygraph/functions/core/k_core.py
@@ -6,6 +6,46 @@ __all__ = [
 ]
 
 
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from easygraph import Graph
+
+
 @hybrid("cpp_k_core")
-def k_core(G):
-    raise EasyGraphError("Please input GraphC or DiGraphC.")
+def k_core(G: "Graph", k: int = 1):
+    # Create a shallow copy of the input graph
+    H = G.copy()
+
+    # Initialize a dictionary to store the degrees of the nodes
+    degrees = dict(H.degree())
+
+    # Repeat until all nodes have degree < k
+    while True:
+        # Find the nodes with degree < k
+        to_remove = [n for n in H.nodes if degrees[n] < k]
+
+        # If there are no such nodes, we're done
+        if not to_remove:
+            break
+
+        # Remove the nodes and their incident edges
+        H.remove_nodes_from(to_remove)
+
+        # Update the degrees of the remaining nodes
+        degrees = dict(H.degree())
+
+    return H
+
+
+def test_k_core():
+    from easygraph import Graph
+
+    G = Graph()
+    G.add_edges_from([(1, 2), (1, 3), (2, 3), (2, 4), (3, 4), (4, 5)])
+    H = k_core(G, k=2)
+    assert sorted(H.nodes.keys()) == sorted([1, 2, 3, 4])
+    assert sorted((x, y) for x, y, _ in H.edges) == sorted(
+        [(1, 2), (1, 3), (2, 3), (2, 4), (3, 4)]
+    )
