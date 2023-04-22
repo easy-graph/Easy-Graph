03865b16964988f46d5ad4d7186f75655bd2c8c4
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-04-22 13:18:30 +0800

    2023-04-22T13-18-30Z

--- setuppy-install.sh
+++ setuppy-install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export PYBIND11_EXT_LIBRARIES='stdc++'
+export PYBIND11_EXT_INCLUDE_DIRS='/usr/local/Cellar/gcc/12.2.0/include/c++/12:/usr/local/Cellar/gcc/12.2.0/include/c++/12/x86_64-apple-darwin21'
+
 CC='/usr/local/Cellar/gcc/12.2.0/bin/gcc-12' CXX='/usr/local/Cellar/gcc/12.2.0/bin/g++-12' python3 setup.py build_ext
 python3 setup.py install --skip-build
 python3 setup.py bdist_wheel
