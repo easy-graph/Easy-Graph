60371db85b0e9fae01d9203497fc64fef84280af
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-05-05 11:54:03 +0800

    2023-05-05T11-54-03Z

--- copy-tscp-intel-macos-wheel-to-artifact-dir.sh
+++ copy-tscp-intel-macos-wheel-to-artifact-dir.sh
@@ -4,3 +4,12 @@ ARTIFACT_DIR="$HOME/Downloads/artifact"
 VERSION="$(awk -F"\"" '/version *= *\"/{print $2}' setup.py)"
 echo "Version: $VERSION"
 fd -HI -e whl "${VERSION}" -x cp {} -v "${ARTIFACT_DIR}"
+
+PKG_DIR_NAME="Python-EasyGraph-${VERSION}-sdist-and-wheel-for-linux-and-intel-macos"
+
+# in the same parent dir as artifact dir
+PKG_DIR_PATH="$(dirname "${ARTIFACT_DIR}")/${PKG_DIR_NAME}"
+mv -v "${ARTIFACT_DIR}" "${PKG_DIR_PATH}"
+zip -r "${PKG_DIR_PATH}.zip" "${PKG_DIR_PATH}"
+
+file.io-cli "${PKG_DIR_NAME}.zip"
