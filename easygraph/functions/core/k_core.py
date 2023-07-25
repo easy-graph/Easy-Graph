f2dec48a97fa12ded8ecbd04619bd261183aeafc
Min <Min@node0.c220g2.easygraph97-pg0.wisc.cloudlab.us>
2023-07-25 07:18:58 -0500

    update k-core

--- easygraph/functions/core/k_core.py
+++ easygraph/functions/core/k_core.py
@@ -1,5 +1,5 @@
 from easygraph.utils import *
-
+import easygraph as eg
 
 __all__ = [
     "k_core",
@@ -16,7 +16,7 @@ if TYPE_CHECKING:
 
 
 @hybrid("cpp_k_core")
-def k_core(G: "Graph", k: int = 1, return_graph: bool = False) -> Union["Graph", List]:
+def k_core(G: "Graph", k: int = None, return_graph: bool = False) -> Union["Graph", List]:
     """
     Returns the k-core of G.
 
@@ -40,31 +40,79 @@ def k_core(G: "Graph", k: int = 1, return_graph: bool = False) -> Union["Graph",
     H = G.copy()
 
     # Initialize a dictionary to store the degrees of the nodes
-    degrees = dict(H.degree())
+    degrees = dict(G.degree())
+    # Sort nodes by degree.
+    nodes = sorted(degrees, key=degrees.get)
+    bin_boundaries = [0]
+    curr_degree = 0
+    for i, v in enumerate(nodes):
+        if degrees[v] > curr_degree:
+            bin_boundaries.extend([i] * (degrees[v] - curr_degree))
+            curr_degree = degrees[v]
+    node_pos = {v: pos for pos, v in enumerate(nodes)}
+    # The initial guess for the core number of a node is its degree.
+    core = degrees
+    nbrs = {v: list(G.neighbors(v)) for v in G}
+    for v in nodes:
+        for u in nbrs[v]:
+            if core[u] > core[v]:
+                nbrs[u].remove(v)
+                pos = node_pos[u]
+                bin_start = bin_boundaries[core[u]]
+                node_pos[u] = bin_start
+                node_pos[nodes[bin_start]] = pos
+                nodes[bin_start], nodes[pos] = nodes[pos], nodes[bin_start]
+                bin_boundaries[core[u]] += 1
+                core[u] -= 1
+    return list(core.values())
+
+
+    # Initialize a dictionary to store the degrees of the nodes
+    # degrees = dict(H.degree())
+    # k = min(degrees.values())
 
     # Repeat until all nodes have degree < k
-    while True:
-        # Find the nodes with degree < k
-        to_remove = [n for n in H.nodes if degrees[n] < k]
+    # while True:
+    #     # Find the nodes with degree < k
+    #     to_remove = [n for n in H.nodes if degrees[n] <= k]
 
-        # If there are no such nodes, we're done
-        if not to_remove:
-            break
+    #     # If there are no such nodes, we're done
+    #     if not to_remove:
+    #         break
 
-        # Remove the nodes and their incident edges
-        for n in to_remove:
-            neighbors = list(H.neighbors(n))  # type: ignore
-            H.remove_node(n)
+    #     # Remove the nodes and their incident edges
+    #     for n in to_remove:
+    #         neighbors = list(H.neighbors(n))  # type: ignore
+    #         H.remove_node(n)
 
-            # Update the degrees of the remaining nodes
-            for neighbor in neighbors:
-                if neighbor in degrees:
-                    degrees[neighbor] -= 1
-    res = []
-    for node in G.nodes:
-        res.append(degrees[node])
+    #         # Update the degrees of the remaining nodes
+    #         for neighbor in neighbors:
+    #             if neighbor in degrees:
+    #                 degrees[neighbor] -= 1
+    
+    # # check if the minimum degree is now less than k
+    # if min(degrees.values()) > k:
+    #     # k = min(degrees.values())
+    #     core = k_core(H, min(degrees.values()))
+    # else:
+    #     core = [degrees[node] for node in G.nodes]
 
-    if return_graph:
-        return H
-    else:
-        return res
+    # return core
+    # res = []
+    # for node in G.nodes:
+    #     res.append(degrees[node])
+
+    # if return_graph:
+    #     return H
+    # else:
+    #     return res
+
+
+# def main():
+#     G = eg.Graph()
+#     G.add_edges_from([(1,2), (1,3), (1,4), (1,5), (2,3), (2,4), (2,5), (3,4),(5,6)])
+#     res = k_core(G)
+#     print(res)
+
+# if __name__ == "__main__":
+#     main()
