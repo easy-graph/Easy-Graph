245a2a4f48a2369933dcebff49684c157ff9d554
yizhihenpidehou <806237230@qq.com>
2024-03-19 10:48:18 +0800

    fixed: hypergraph_geenrator -> null_model

--- easygraph/functions/hypergraph/__init__.py
+++ easygraph/functions/hypergraph/__init__.py
@@ -1,5 +1,5 @@
 from .assortativity import *
 from .centrality import *
 from .hypergraph_clustering import *
-from .null_model import *
 from .hypergraph_operation import *
+from .null_model import *
