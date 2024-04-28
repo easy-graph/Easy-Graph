5592495256ad305b9a46fb028ae7cac8866426e1
yizhihenpidehou <806237230@qq.com>
2024-04-28 14:30:20 +0800

    fixed: torch dependency check

--- easygraph/classes/__init__.py
+++ easygraph/classes/__init__.py
@@ -1,19 +1,19 @@
-# try:
-from .base import BaseHypergraph
-from .base import load_structure
-from .directed_graph import DiGraph
-from .directed_graph import DiGraphC
-from .directed_multigraph import MultiDiGraph
-from .graph import Graph
-from .graph import GraphC
-from .graphviews import *
-from .hypergraph import Hypergraph
-from .multigraph import MultiGraph
-from .operation import *
+try:
+    from .base import BaseHypergraph
+    from .base import load_structure
+    from .directed_graph import DiGraph
+    from .directed_graph import DiGraphC
+    from .directed_multigraph import MultiDiGraph
+    from .graph import Graph
+    from .graph import GraphC
+    from .graphviews import *
+    from .hypergraph import Hypergraph
+    from .multigraph import MultiGraph
+    from .operation import *
 
 
-# except:
-#     print(
-#         "Warning raise in module:classes. Please install Pytorch before you use"
-#         " functions related to Hypergraph"
-#     )
+except:
+    print(
+        "Warning raise in module:classes. Please install Pytorch before you use"
+        " functions related to Hypergraph"
+    )
