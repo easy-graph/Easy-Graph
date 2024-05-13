97f6bffe3b8c01c34cb09773970f550ef345f389
mgao97 <mgao21@m.fudan.edu.cn>
2024-05-13 17:25:16 +0800

    optimize hyperdegree

--- easygraph/functions/hypergraph/centrality/hyperdegree.py
+++ easygraph/functions/hypergraph/centrality/hyperdegree.py
@@ -11,18 +11,25 @@ def hypergraph_degree_centrality(G):
 
     Returns
     ----------
-    degree centrality of each node in G : dict
+    hyperdegree of each node in G : dict
 
     """
-    res = {}
     node_list = G.v
-    # Get hyperedge list
     edge_list = G.e[0]
-    for node in node_list:
-        res[node] = 0
+
+    res = {node: 0 for node in node_list}
 
     for e in edge_list:
-        for n in e:
-            res[n] += 1
+        res.update({node: res[node] + 1 for node in e})
+    # res = {}
+    # node_list = G.v
+    # # Get hyperedge list
+    # edge_list = G.e[0]
+    # for node in node_list:
+    #     res[node] = 0
+
+    # for e in edge_list:
+    #     for n in e:
+    #         res[n] += 1
 
     return res
