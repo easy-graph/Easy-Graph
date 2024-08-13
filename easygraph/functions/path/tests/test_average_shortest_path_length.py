76f5406c5daacd2ec966098202acd4d0f1c15000
cocowater5747 <kcheng0331@gmail.com>
2024-08-13 18:08:52 +0800

    Added tests to functions\path.

---
+++ easygraph/functions/path/tests/test_average_shortest_path_length.py
@@ -0,0 +1,12 @@
+import unittest
+
+import easygraph as eg
+
+
+class test_average_shortested_path_length(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ easygraph/functions/path/tests/test_bridges.py
@@ -0,0 +1,46 @@
+import unittest
+
+import easygraph as eg
+
+
+class test_bridges(unittest.TestCase):
+    def setUp(self):
+        self.g1 = eg.get_graph_karateclub()
+
+        # source graph: https://zh.wikipedia.org/zh-cn/%E6%88%B4%E5%85%8B%E6%96%AF%E7%89%B9%E6%8B%89%E7%AE%97%E6%B3%95#/media/File:Dijkstra_Animation.gif
+        edges = [(1, 2), (1, 3), (1, 6), (2, 3), (2, 4), (3, 4), (3, 6), (4, 5), (5, 6)]
+        self.g2 = eg.Graph(edges)
+        self.g2.add_edges(edges, edges_attr=[{"weight": 7}, {"weight": 9}, {"weight": 14}, {"weight": 10},
+                                             {"weight": 15}, {"weight": 11}, {"weight": 2}, {"weight": 6},
+                                             {"weight": 9}])
+
+        # source graph: https://static.javatpoint.com/tutorial/daa/images/dijkstra-algorithm.png
+        self.g3 = eg.Graph()
+        edges = [(0, 1), (0, 4), (1, 4), (1, 2), (4, 5), (4, 8), (2, 3), (2, 6), (2, 8), (5, 6), (5, 8), (3, 6), (3, 7),
+                 (6, 7)]
+
+        self.g3.add_edges(edges, edges_attr=[{"weight": 4}, {"weight": 1}, {"weight": 11}, {"weight": 8},
+                                             {"weight": 1}, {"weight": 7}, {"weight": 7}, {"weight": 4},
+                                             {"weight": 2}, {"weight": 2}, {"weight": 6}, {"weight": 14},
+                                             {"weight": 9}, {"weight": 10}])
+        self.g4 = eg.Graph()
+        edges = [(0, 1), (1, 2), (2, 3), (3, 0), (0, 2), (1, 3)]
+        self.g4.add_edges(edges, edges_attr=[{'weight': -1}, {'weight': -2}, {'weight': -3}, {'weight': -4},
+                                             {'weight': -5}, {'weight': -6}])
+
+    def result(self, g: eg.Graph):
+        res = eg.bridges(g)
+        for i in res:
+            print(i)
+
+    def test_bridges(self):
+        self.result(g=self.g2)
+        self.result(g=self.g3)
+        self.result(g=self.g4)
+
+    def test_has_bridges(self):
+        print(eg.has_bridges(self.g2))
+
+
+if __name__ == '__main__':
+    unittest.main()
