ceda97fa4595bdd34ef6ac7115fb10182178895f
yzhpdh <61486008+yizhihenpidehou@users.noreply.github.com>
2025-05-26 21:22:46 +0800

    chore: Upgrade version

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ with open("README.md") as fh:
 CYTHON_STR = "Cython"
 setuptools.setup(
     name="Python-EasyGraph",
-    version="1.4.4",
+    version="1.4.5",
     author="Fudan DataNET Group",
     author_email="mgao21@m.fudan.edu.cn",
     description="Easy Graph",
