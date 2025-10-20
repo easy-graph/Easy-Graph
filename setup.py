cb0c8fb9731cddf23286909d3f1342a7010de494
acture <acturea@gmail.com>
2025-10-21 02:07:15 +0800

    style(packaging): Use SPDX license identifier 'BSD-3-Clause'
    The `setup.py` file used the deprecated license classifier
    `License :: OSI Approved :: BSD License`. Modern Python packaging standards
    (PyPA) recommend using specific, machine-readable SPDX identifiers.
    
    This commit replaces the old classifier with the precise `BSD-3-Clause`
    identifier in the `license` field. This aligns the project with best
    practices and silences setuptools deprecation warnings.

--- setup.py
+++ setup.py
@@ -125,7 +125,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: 3.14",
-        "License :: OSI Approved :: BSD License",
+        "BSD-3-Clause",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8, <3.15",
