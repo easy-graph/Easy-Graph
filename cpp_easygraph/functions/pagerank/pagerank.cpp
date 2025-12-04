90af1ad70a0e00f0a8341d976c97b547e5de4313
sama <sama@node0.wyz2.hrsama-pg0.utah.cloudlab.us>
2025-12-03 20:28:02 -0700

    omp

--- cpp_easygraph/functions/pagerank/pagerank.cpp
+++ cpp_easygraph/functions/pagerank/pagerank.cpp
@@ -1,18 +1,18 @@
+#include <omp.h>
+#include <vector>
+#include <cmath>
+#include <cstdio>
 #include "pagerank.h"
 #include "../../classes/directed_graph.h"
 #include "../../common/utils.h"
 #include "../../classes/linkgraph.h"
-#include "time.h"
 
 struct Page {
-	Page(){} 
+    Page(){} 
     Page(const double &_newPR, const double &_oldPR) {newPR = _newPR; oldPR = _oldPR;}
 
     double newPR, oldPR;
 };
-
-// outDegree
-// get_edge_from_node
 
 py::object _pagerank(py::object G, double alpha=0.85, int max_iterator=500, double threshold=1e-6) {
 
@@ -23,10 +23,10 @@ py::object _pagerank(py::object G, double alpha=0.85, int max_iterator=500, doub
     }
     DiGraph& G_ = G.cast<DiGraph&>();
     int N = G_.node.size();
-    // Graph_L G_l = graph_to_linkgraph(G_, is_directed, "", true);
+    
     Graph_L G_l;
     if(G_.linkgraph_dirty){
-        G_l = graph_to_linkgraph(G_, is_directed, "", true);
+        G_l = graph_to_linkgraph(G_, is_directed, "", true, false);
         G_.linkgraph_structure=G_l;
         G_.linkgraph_dirty = false;
     }
@@ -38,30 +38,43 @@ py::object _pagerank(py::object G, double alpha=0.85, int max_iterator=500, doub
     std::vector<int> outDegree = G_l.degree;
     std::vector<int> head = G_l.head;
 
-    std::vector<Page>page(N+1);
+    std::vector<Page> page(N+1);
+    
+    #pragma omp parallel for
     for (int i = 1; i < N + 1; ++i) {
         page[i] = Page(0, 1.0/N);
     }
 
-    int cnt = 0; //统计迭代几轮
-	int shouldStop = 0; //根据oldPR与newPR的差值 判断是否停止迭代
-
+    int cnt = 0; 
+    int shouldStop = 0; 
 
     while(!shouldStop)
     {
         shouldStop = 1;
         double res = 0;
+
+        #pragma omp parallel for reduction(+:res)
         for(int i = 1; i < N+1; ++i) {
             if (outDegree[i] == 0) {
                 res += page[i].oldPR;
-                continue;
             }
+        }
+
+        #pragma omp parallel for schedule(dynamic, 128)
+        for(int i = 1; i < N+1; ++i) {
+            if (outDegree[i] == 0) continue; 
+            
             double tmpPR = (page[i].oldPR / outDegree[i]) * alpha;
+            
             for(int p = head[i]; p != -1; p = E[p].next){
+                #pragma omp atomic
                 page[E[p].to].newPR += tmpPR;
             }
         }
+
         double sum = 0;
+
+        #pragma omp parallel for reduction(+:sum)
         for(int i = 1; i < N+1; ++i)
         {
             page[i].newPR += (1 - alpha) / N + res / N * alpha;
