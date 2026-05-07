f934080c29a8affc4e782d345de86f3ec25bad4a
Haoran Du <d1325130388@gmail.com>
2026-05-07 14:20:13 +0800

    Bump version from 1.6 to 1.6.1

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ with open("README.md", encoding="utf-8") as fh:
 CYTHON_STR = "Cython"
 setuptools.setup(
     name="Python-EasyGraph",
-    version="1.6",
+    version="1.6.1",
     author="Fudan DataNET Group",
     author_email="mgao21@m.fudan.edu.cn",
     description="Easy Graph",
